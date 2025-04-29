439d3f2f5b268a4b74fa30c93550adb955feaa9d
kate <kata3140@gmail.com>
2025-04-29 14:42:14 +0300

    new

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,6 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './store';
 
-const rootElement = document.querySelector('#root');
-
-// ReactDOM.render(<App />, rootElement);
 ReactDOM.render(
     <Provider store={store}>
         <App />
--- src/store/workersSlice.js
+++ src/store/workersSlice.js
@@ -3,27 +3,27 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const baseUrl = 'https://66a0f8b17053166bcabd894e.mockapi.io/api/workers';
 
 export const fetchWorkers = createAsyncThunk('workers/fetchWorkers', async function () {
-    const response = await fetch(baseUrl);
-    const data = await response.json();
-    return data;
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+  return data;
 });
 
 const workersSlice = createSlice({
-    name: 'workers',
-    initialState: {
-        workers: [],
-        status: null,
-    },
-    extraReducers(builder) {
-        builder
-            .addCase(fetchWorkers.pending, state => {
-                state.status = 'in progress';
-            })
-            .addCase(fetchWorkers.fulfilled, (state, action) => {
-                state.status = 'succes';
-                state.workers = action.payload;
-            });
-    },
+  name: 'workers',
+  initialState: {
+    workers: [],
+    status: null,
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchWorkers.pending, state => {
+        state.status = 'in progress';
+      })
+      .addCase(fetchWorkers.fulfilled, (state, action) => {
+        state.status = 'succes';
+        state.workers = action.payload;
+      });
+  },
 });
 
 export const { filterPositionAnalyst } = workersSlice.actions;
